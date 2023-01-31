a744b0e3392efec3c7b11544285729306e07f685
Eleanor Kelman <ekelman3@gmail.com>
2023-01-31 17:03:12 -0500

    finished basic main page css

--- src/components/TeamEdward.js
+++ src/components/TeamEdward.js
@@ -8,6 +8,18 @@ function TeamEdward() {
         <div className = 'team'>
             <img src = "./fonts/teamedward_header.gif" alt = "we are team edward" />
             <RandomQuote />
+            <div className = 'align-left'>
+                <p>
+                    We are team Edward.
+                    We sparkle in the sunlight, we live many human lifetimes, and we drink animal blood.
+                    We are misunderstood by humanity, but we continue our struggle in darkness.
+                    We are vampires.
+                    We are team Edward.
+                </p>
+            </div>
+            <div className = 'align-right'>
+                
+            </div>
         </div>
     )
 }
--- src/components/TeamJacob.js
+++ src/components/TeamJacob.js
@@ -7,6 +7,18 @@ function TeamJacob() {
         <div className = 'team'>
             <img src = "./fonts/teamjacob_header.gif" alt = "we are team jacob" />
             <RandomQuote />
+            <div className = 'align-left'>
+                <p>
+                    We are team Jacob.
+                    We howl at the moon, we transform under the cover of darkness, and we follow the alpha.
+                    We know we must be lone wolves, but have the protection of the pack.
+                    We are werewolves.
+                    We are team Jacob.
+                </p>
+            </div>
+            <div className = 'align-right'>
+                
+            </div>
         </div>
     )
 }
