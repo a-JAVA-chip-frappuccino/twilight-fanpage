6abc485ada39e88af558bf8d944b725db5175a5a
Eleanor Kelman <ekelman3@gmail.com>
2023-02-07 17:44:12 -0500

    randomized number of droplets

--- src/index.js
+++ src/index.js
@@ -4,26 +4,26 @@ import App from "./components/App";
 import "./index.css";
 
 document.addEventListener("DOMContentLoaded", function() {
-   
-    var amount = 30;
-    var body = document.querySelector('body');
-    var i = 0;
-    
-    while ( i < amount ) {
 
-      var node = document.createElement("i");
-      var posX = Math.floor(Math.random() * window.innerWidth);
-      var posY = Math.floor(Math.random() * window.innerHeight);
-      var rotation = Math.random() * 180;
-      var delay = Math.random() * 20;
-      var scale = Math.random() * 0.2;
+    const totalDroplets = Math.floor(Math.random() * 50) + 20;
+    
+    let i = 0;
+    
+    while ( i < totalDroplets ) {
+
+      const node = document.createElement("i");
+      const posX = Math.floor(Math.random() * window.innerWidth);
+      const posY = Math.floor(Math.random() * window.innerHeight);
+      const rotation = Math.random() * 180;
+      const delay = Math.random() * 20;
+      const scale = Math.random() * 0.2;
       node.style.left = posX + 'px';
       node.style.top = posY + 'px';
       node.style.transform = 'rotate(' + rotation + 'deg) scale(' + scale + ')';
       node.style.animationDelay = delay + 's';
-      body.appendChild(node);
+      document.querySelector('body').appendChild(node);
       i++;
-      
+
     }
     
   });
