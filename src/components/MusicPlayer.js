c678862b793ed1007dd74a16d20f78546a56f8d7
Eleanor Kelman <ekelman3@gmail.com>
2023-02-12 10:25:12 -0500

    finished button css

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,28 +1,24 @@
 import React, { useState } from "react";
 
 const song = new Audio("../sounds/supermassiveblackhole-muse.mp3");
+song.volume = 0.2;
 
 function MusicPlayer() {
 
     const [playMusic, setPlayMusic] = useState(false);
 
-    console.log(playMusic);
-
     function handleClick() {
         setPlayMusic(!playMusic)
     }
 
-    if (playMusic) {
-        song.volume = 0.2;
-        song.play();
-    }
-    else {
-        song.pause();
-    }
+    playMusic ? song.play() : song.pause();
 
     return (
-        <div>
+        <div className = 'player'>
+            <label className = 'arrow'></label>
             <button onClick = { handleClick }>{ playMusic ? "Pause BG Music" : "Play BG Music" }</button>
+            <br />
+            <br />
         </div>
     )
 }
